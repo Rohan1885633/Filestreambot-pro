0eaf006dd477c07607df692fa80fce84e5f8327b
Rohan1885633 <141541061+Rohan1885633@users.noreply.github.com>
2023-12-15 09:19:58 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -36,5 +36,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'ez6mpft.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', '-1002077875913'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'The_Movies_Officially'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
